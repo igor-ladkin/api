44b6e40e548546a715ea139186f79e6811cbd971
Igor Ladkin <dgkplan@googlemail.com>
2016-07-12 16:47:53 +0900

    Config for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,14 @@ use Mix.Config
 # which you typically run after static files are built.
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "sheltered-bayou-16057.herokuapp.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :peepchat, Peepchat.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
